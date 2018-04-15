0a18b2e356f0f138f824c9a3021b5d51d674be06
Keith O'Hara <keith.ohara@gmail.com>
2018-04-15 19:43:35 -0400

    Create sketch.js

---
+++ sketch.js
@@ -0,0 +1,26 @@
+// Keith O'Hara <kohara@bard.edu>
+// http://www.nahee.com/spanky/www/fractint/circle_type.html
+
+var s = 0.93980;
+var y = 0; 
+var x = 0;
+
+
+function setup(){
+  createCanvas(window.innerWidth, window.innerHeight);
+  fill(0);
+  background(255);
+}
+
+
+function draw(){
+  if (y < height){
+    for (var x = 0; x < width; x++){
+      var z = Math.floor(s*(x*x + y*y));
+      if (z % 2 === 0){
+        point(x, y);
+      }
+    }
+    y = y + 1;
+  }
+}
